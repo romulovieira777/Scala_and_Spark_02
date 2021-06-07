e4990ece0e78ce5e60997082fa9a1f8048a9592e
Rômulo Vieira <romulo.vieira777@gmail.com>
2021-06-07 10:14:30 -0300

    Learn Scala for Spark

---
+++ Seção 02 - Learn Scala for Spark/Scala Functions - Operators.scala
@@ -0,0 +1,25 @@
+// Declarando variáveis
+
+val a = 2
+val b = 3
+
+a + b
+a.+(b)
+
+val a = true
+val b = false
+
+b && a
+b.&&(a)
+
+b.||(a)
+b || a
+
+a.==(b)
+a == b
+
+a equals b
+a.equals(b)
+
+a != b
+a.!=(b)
